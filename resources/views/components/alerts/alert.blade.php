59936dc62cd9e12a5d8e601ab9a16414ac86f75d
Feras Barahmeh <ferasbarahmeh55@gmail.com>
2023-12-31 20:57:36 +0300

    add scroll to alter

--- resources/views/components/alerts/alert.blade.php
+++ resources/views/components/alerts/alert.blade.php
@@ -6,6 +6,9 @@
 ])
 
 @php
+
+    $scroll = $success != null || $fail != null;
+
     if ($success !=null)
         $type = 'success';
     elseif ($fail != null)
@@ -14,6 +17,7 @@
 
 @if(isset($success) || isset($fail))
     <p
+        id="alert"
         x-data="{ show: true }"
         x-show="show"
         x-transition
@@ -21,3 +25,18 @@
         class="text-sm text-gray-600 dark:text-gray-400 text-capitalize alert alert-{{ $type }}"
     >{{ $success ?? $fail }}</p>
 @endif
+
+<script>
+
+    if (@js($scroll)) {
+        let message = document.getElementById('alert');
+        message.scrollIntoView({
+            behavior: 'smooth',
+            block: 'start',
+            inline: 'nearest',
+        });
+    } else {
+        console.log("Not Scroll")
+    }
+
+</script>
