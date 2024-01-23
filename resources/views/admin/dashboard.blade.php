c826c4fc0fb7e35571a66193f723763a8212610a
Feras Barahmeh <ferasbarahmeh55@gmail.com>
2024-01-23 18:54:17 +0300

    set cancelBtn prop in add draft form

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -27,7 +27,7 @@
             <div class="quick-draft p-20 bg-white rad-10">
                 <h2 class="mt-0 mb-10">Quick Draft</h2>
                 <p class="mt-0 mb-20 c-grey fs-15">Write A Draft For Your Ideas</p>
-                <x-admin.add-draft-form />
+                <x-admin.add-draft-form :cancelBtn="false"/>
             </div>
             <!-- End Quick Draft Widget -->
 
